6f10d3b1074b6ca090a00d592843a0226a27d87a
mdczaplicki <mdczaplicki@gmail.com>
2015-05-25 21:19:47 +0200

    Update with Roberts Cross

--- main.py
+++ main.py
@@ -25,3 +25,31 @@ plt.show()
 
 im_g = image.convert('L')
 im_g.save("a.png", "PNG")
+
+def edges():
+    out_img = Image.new("L", f.size, None)
+    img_data = f.load()
+    out_data = out_img.load()
+ 
+    matrix_x = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
+    matrix_y = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]]
+    matrix_size = 3
+    matrix_middle = matrix_size/2
+ 
+    rows, cols = f.size
+ 
+    for row in range(rows - matrix_size):
+        for col in range(cols - matrix_size):
+            pixel_x = 0
+            pixel_y = 0
+            for i in range(matrix_size):
+                for j in range(matrix_size):
+                    val = sum(img_data[row+i, col+j])/3
+                    pixel_x += matrix_x[i][j] * val
+                    pixel_y += matrix_y[i][j] * val
+ 
+            new_pixel = math.sqrt(pixel_x**2 + pixel_y**2)
+            new_pixel = int(new_pixel)
+            out_data[row + matrix_middle, col + matrix_middle] = new_pixel
+ 
+    out_img.show()
